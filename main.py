43a679f6a7c90e8a4ce5493213a21c2efdb1d52f
mbronico <michaelbronico1@gmail.com>
2025-09-03 23:51:07 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+ip=input("Enter IP Address: ")
+netAdd=("0.0.0.1")
+firstHost=("0.0.0.2")
+LastHost=("0.0.0.3")
+Broadcast=("0.0.0.4")
+nextNet=("0.0.0.5")
+
+
+print("""
+      IP Address: {}
+      Network Address: {}
+      First Host: {}
+      Last Host: {}
+      Broadcast Address: {}
+      Next Network Address: {}
+      """.format(ip, netAdd, firstHost, LastHost, Broadcast, nextNet))
