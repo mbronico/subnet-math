1a7bd275c935457ac9d5a303aaba9c69314cf8c8
mbronico <michaelbronico1@gmail.com>
2025-09-04 00:46:29 -0400

    added increment chart

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-
-
-
 ip=input("Enter IP Address: ")
+
+splitter=ip.split("/")
+subnet_mask=int(splitter[1])
+workable_octets=splitter[0].split(".")
+
+incrementor = ("Subnet mask not in range.")
+
+
+if subnet_mask in (1, 9, 17, 25):
+    incrementor = 128
+elif subnet_mask in (2, 10, 18, 26):
+    incrementor = 64
+elif subnet_mask in (3, 11, 19, 27):
+    incrementor = 32
+elif subnet_mask in (4, 12, 20, 28):
+    incrementor = 16
+elif subnet_mask in (5, 13, 21, 29):
+    incrementor = 8
+elif subnet_mask in (6, 14, 22, 30):
+    incrementor = 4
+elif subnet_mask in (7, 15, 23, 31):
+    incrementor = 2 
+elif subnet_mask in (8, 16, 24, 32):
+    incrementor = 1
+
+
+
+print(workable_octets)
+print(subnet_mask)
+print(incrementor)
+
+
 net_add=("0.0.0.1")
 first_host=("0.0.0.2")
 last_host=("0.0.0.3")
