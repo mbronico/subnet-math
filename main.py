b6296c2487d5c74c27633c6d03b5964654834607
mbronico <michaelbronico1@gmail.com>
2025-09-03 23:56:32 -0400

    switched to snake_case

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+
+
+
 ip=input("Enter IP Address: ")
-netAdd=("0.0.0.1")
-firstHost=("0.0.0.2")
-LastHost=("0.0.0.3")
-Broadcast=("0.0.0.4")
-nextNet=("0.0.0.5")
+net_add=("0.0.0.1")
+first_host=("0.0.0.2")
+last_host=("0.0.0.3")
+broadcast=("0.0.0.4")
+next_net=("0.0.0.5")
 
 
 print("""
       IP Address: {}
+
       Network Address: {}
       First Host: {}
       Last Host: {}
       Broadcast Address: {}
       Next Network Address: {}
-      """.format(ip, netAdd, firstHost, LastHost, Broadcast, nextNet))
+      """.format(ip, net_add, first_host, last_host, broadcast, next_net))
